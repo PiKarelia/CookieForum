fb218c250c3728efa0a8d4e8535e4c5e029d25ff
Ana Grigorcea <ana.grigorcea@gmail.com>
2020-02-17 23:51:56 +0200

    Added API and Data project

---
+++ CookieForum.Data/Entities/Inquiry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookieForum.Data.Entities
+{
+	public class Inquiry
+	{
+		public string Caption { get; set; }
+	}
+}
